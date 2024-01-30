1b020aefc3edfc798b0a1b6417209f1ddea6cec4
Ananya <ananyasadhukhan68439@gmail.com>
2024-01-31 01:00:58 +0530

    Initial commit

---
+++ static/js/cart.js
@@ -0,0 +1,30 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (let i = 0; i < updateBtns; i++) {
+    updateBtns[i].addEventListener('click',function(){
+        var productId = this.dataset.product
+        var action =  this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+
+        console.log('USER:',user )
+        if (user == 'AnomyomusUser') {
+            console.log('Not logged in')
+        } else {
+            console.log('User is logged in , sending data')
+        }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log('user is authenticated, sending data ...')
+        var url = '/update_item/'
+        fetch(url, {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+                'X-CSRFToken':csrftoken,
+            },
+            body: JSON.stringify({'productId':productId,'action':action,})
+})
+.then((response)=>{return response.json()})
+.then((data) =>{ console.log('Data', data)})
